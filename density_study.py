f01c3ebd1a34b4f1a555bf5fdbc1052263b6e1e2
Muhamamd Talha Sadaqat <97758518+talha-sadaqat@users.noreply.github.com>
2023-08-10 18:38:12 -0400

    Create density_study.py

---
+++ density_study.py
@@ -0,0 +1,19 @@
+def show_density(var_data):
+    fig = plt.figure(figsize=(10,4))
+
+    # Plot density
+    var_data.plot.density()
+
+    # Add titles and labels
+    plt.title('Data Density')
+
+    # Show the mean, median, and mode
+    plt.axvline(x=var_data.mean(), color = 'cyan', linestyle='dashed', linewidth = 2)
+    plt.axvline(x=var_data.median(), color = 'red', linestyle='dashed', linewidth = 2)
+    plt.axvline(x=var_data.mode()[0], color = 'yellow', linestyle='dashed', linewidth = 2)
+
+    # Show the figure
+    plt.show()
+
+# Get the density of StudyHours
+show_density(col)
